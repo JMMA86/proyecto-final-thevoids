6f08fc746530ffbd167a61e1017bf9dd61755970
JMMA86 <1126597295@u.icesi.edu.co>
2025-03-24 19:02:32 -0500

    test: Added controller and main tests

--- src/test/java/org/thevoids/oncologic/OncoLogicApplicationTests.java
+++ src/test/java/org/thevoids/oncologic/OncoLogicApplicationTests.java
@@ -8,6 +8,12 @@ class OncoLogicApplicationTests {
 
     @Test
     void contextLoads() {
+        // Verifica que el contexto de la aplicación se carga correctamente
     }
 
+    @Test
+    void main() {
+        // Verifica que el método main se ejecuta sin lanzar excepciones
+        OncoLogicApplication.main(new String[] {});
+    }
 }
