3eff1981cceaec626658c2dbbf51646d653870bf
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2025-04-16 15:00:57 -0500

    fix: Now the default page redirects to /web/login

---
+++ src/main/java/org/thevoids/oncologic/controller/Home.java
@@ -0,0 +1,12 @@
+package org.thevoids.oncologic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/web/home";
+    }
+}
